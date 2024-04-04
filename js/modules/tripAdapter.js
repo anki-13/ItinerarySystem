6309b2cdbcebe6ebe00aa0cf4f2f551bdd043919
Abiral Timalsina <68950365+abiraltmlcena@users.noreply.github.com>
2024-04-04 07:01:14 +0100

    Update tripAdapter.js

--- js/modules/tripAdapter.js
+++ js/modules/tripAdapter.js
@@ -8,7 +8,7 @@ export default class TripAdapter {
   trips = [];
 
   constructor() {
-      this.baseUrl = "http://localhost:3000";
+      this.baseUrl = "http://localhost:3000/trips";
   }
 
   fetchTrips() {
